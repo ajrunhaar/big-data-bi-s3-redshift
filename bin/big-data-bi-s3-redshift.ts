9012325dcefddc43e366fe592e2be8dfb3325a74
Anton Runhaar <anton.runhaar@shellstack.nl>
2020-04-13 19:20:26 +0200

    Added infrastructure - Redshift, Lambda, S3 as well a boilerplate Lambda handler

--- bin/big-data-bi-s3-redshift.ts
+++ bin/big-data-bi-s3-redshift.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { BigDataBiS3RedshiftStack } from '../lib/big-data-bi-s3-redshift-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { BigDataBiS3RedshiftStack } from "../lib/big-data-bi-s3-redshift-stack";
 
 const app = new cdk.App();
-new BigDataBiS3RedshiftStack(app, 'BigDataBiS3RedshiftStack');
+new BigDataBiS3RedshiftStack(app, "BigDataBiS3RedshiftStack");
